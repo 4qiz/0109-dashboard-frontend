ed0c8d98ab7a55f35d9067e81bab84e0f3949b33
4qiz <egor.antipin.1995mail.ru@gmail.com>
2026-03-09 20:31:41 +0300

    fix cringe redirect

--- nextapp/src/shared/constants/app-routes.ts
+++ nextapp/src/shared/constants/app-routes.ts
@@ -1,14 +1,14 @@
 export const APP_BASE_URL = process.env.NEXT_PUBLIC_APP_URL ?? "";
 
 export const appRoutes = {
-  home: () => `/`,
-  login: "/login",
-  clusters: () => `/clusters`,
-  clustersId: (id: number) => `/clusters/${id}`,
+  home: () => `${APP_BASE_URL}/`,
+  login: `${APP_BASE_URL}/login`,
+  clusters: () => `${APP_BASE_URL}/clusters`,
+  clustersId: (id: number) => `${APP_BASE_URL}/clusters/${id}`,
   machine: (clusterId: number, machineId: number) =>
-    `/clusters/${clusterId}/machines/${machineId}`,
+    `${APP_BASE_URL}/clusters/${clusterId}/machines/${machineId}`,
   disk: (clusterId: number, machineId: number, diskId: number) =>
-    `/clusters/${clusterId}/machines/${machineId}/disks/${diskId}`,
+    `${APP_BASE_URL}/clusters/${clusterId}/machines/${machineId}/disks/${diskId}`,
 
   apiLogin: `${APP_BASE_URL}/publicapi/auth/login`,
   apiRefresh: `${APP_BASE_URL}/publicapi/auth/refresh`,
