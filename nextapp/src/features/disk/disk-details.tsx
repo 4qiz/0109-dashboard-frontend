e98a4277ac78352df0f26310b2c439cfeafa1bfc
4qiz <egor.antipin.1995mail.ru@gmail.com>
2026-04-02 19:43:47 +0300

    FIX

--- nextapp/src/features/disk/disk-details.tsx
+++ nextapp/src/features/disk/disk-details.tsx
@@ -32,7 +32,7 @@ export const DiskDetails = ({ disk }: { disk: DiskDto }) => {
         icon={<Disc3 className="h-6 w-6 text-primary shrink-0" />}
       />
 
-      <div className=" px-4 py-6 space-y-6">
+      <div className=" px-6 py-6 space-y-6">
         {/* Disk Summary */}
         <div className="space-y-4">
           <div className="flex items-center gap-2">
